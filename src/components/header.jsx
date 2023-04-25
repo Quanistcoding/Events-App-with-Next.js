ef12cc56553bbf370229e93568432250e752df8a
unknown <jenchihhung@hotmail.com>
2023-04-25 09:10:16 +0800

    Added events page.

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -14,7 +14,7 @@ export default function Header(){
                 </Link>
               </li>
               <li>
-                <Link href="/">
+                <Link href="/events">
                    Events
                 </Link>
               </li>
