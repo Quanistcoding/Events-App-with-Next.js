4a56951f53d1f6dbb82037613277b4e69205bb9b
unknown <jenchihhung@hotmail.com>
2023-04-25 09:23:53 +0800

    Populate home page with event categories.

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,12 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Header(){
   return (
     <header>
       <div>
         <div className="topNav">
-          
+          <Image alt="logo" src={'/images/logo_black.png'} width={50} height={50} />          
           <nav>
             <ul>
               <li>
