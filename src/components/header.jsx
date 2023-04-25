f74be0ad98411c6edba56cf2a99536d8a8ee2494
unknown <jenchihhung@hotmail.com>
2023-04-25 09:06:14 +0800

    Added layout.

---
+++ src/components/header.jsx
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+
+export default function Header(){
+  return (
+    <header>
+      <div>
+        <div className="topNav">
+          
+          <nav>
+            <ul>
+              <li>
+                <Link href="/">
+                   Home
+                </Link>
+              </li>
+              <li>
+                <Link href="/">
+                   Events
+                </Link>
+              </li>
+              <li>
+                <Link href="/">
+                    About us
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <p className="title"> Sed ut perspiciatis unde omnis</p>
+      </div>
+    </header>
+  );
+};
